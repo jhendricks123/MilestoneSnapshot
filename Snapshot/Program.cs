8e065a634e439fd97d028d4e6d8d844944c2b999
Josh Hendricks <jhendricks123@gmail.com>
2019-03-05 13:47:17 -0800

    Cleanup usings

--- Snapshot/Program.cs
+++ Snapshot/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommandLine;
+﻿using CommandLine;
 using MipSdkHelper;
-using VideoOS.Platform.Live;
+using System;
+using System.IO;
 
 namespace Snapshot
 {
